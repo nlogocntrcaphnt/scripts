9af495b7409d39486fb88ecc359da28bc17445b7
Ѳад <45270032+nlogocntrcaphnt@users.noreply.github.com>
2021-07-13 15:46:07 +0300

    Update images_download.sh

--- images_download.sh
+++ images_download.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-gallery-dl -d /home/polyphemus/Pictures/ -R 5 --verbose $1
+gallery-dl -d /media/polyphemus/C802F15202F145CA/Pictures/ -R 5 --verbose $1
 
-for file in /home/kostis/Pictures/instagram/*; do
-	username="$(echo "${file##*/}")"
+for file in "/media/polyphemus/C802F15202F145CA/Pictures/instagram/"*
+do
+	username="$(echo $(basename $file))"
 	echo $username
-	gallery-dl -d /home/polyphemus/Pictures/ -R 5 --verbose https://www.instagram.com/$username
+	gallery-dl -d /media/polyphemus/C802F15202F145CA/Pictures/ -R 5 --verbose --sleep 6 https://www.instagram.com/$username
+	python3 /home/polyphemus/Documents/github_clones/PyInstaStories/pyinstastories.py --download $username --output /media/polyphemus/C802F15202F145CA/Pictures/instagram --username [REDACTED] --password [REDACTED]
 done
+
+
+#pip install git+https://git@github.com/ping/instagram_private_api.git@1.6.0
+#python3 -m pip install -U gallery-dl
