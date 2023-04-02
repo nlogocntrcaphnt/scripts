bedd19d42f446fb1ce638b5306b6da9f34cb0af5
Ѳад <45270032+nlogocntrcaphnt@users.noreply.github.com>
2023-04-02 20:19:20 +0300

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -1,13 +1,24 @@
-sudo add-apt-repository ppa:oibaf/graphics-drivers
-sudo apt update
-sudo apt upgrade
-sudo apt install libvulkan1 mesa-vulkan-drivers vulkan-utils
+mkdir Pictures/screenshots
+mkdir Music/Artists
 
-sudo apt install git
+sudo pacman -S Syyu
+
+#enable bitmap fonts
+sudo rm /etc/fonts/conf.d/70-no-bitmaps.conf
+
+sudo pacman -S git
+
+mkdir Pictures/screenshots
+
 cd Documents
 mkdir repositories
 cd repositories
+
 git clone https://github.com/nlogocntrcaphnt/dotfiles.git
+git clone https://github.com/nlogocntrcaphnt/scripts.git
+git clone https://github.com/nlogocntrcaphnt/Homepage.git
+git clone https://github.com/nlogocntrcaphnt/LaTeX_personal_dictionary.git
+
 cd dotfiles
 
 mv config .config
@@ -31,57 +42,19 @@ cp .rtorrent.rc /home/polyphemus/
 cp .Xresources /home/polyphemus/
 cp wallpaper_test.jpeg ~/Pictures/
 
-cd
+#essentials
+sudo pacman -S i3-gaps i3status i3lock dmenu rxvt-unicode xclip maim neovim feh
+sudo pacman -S base-devel python curl python-setuptools rsync
 
-sudo apt install neovim mpd ncmpcpp mpc rxvt-unicode newsboat dmenu xclip maim feh rsync i3status i3lock autoconf make xutils-dev build-essential ffmpeg task-spooler audacity neofetch ffmpegthumbnailer zathura lxappearance rtorrent sdcv texlive-latex-extra openssh-server python curl python3-pip
-xrdb .Xresources
+#media handling
+sudo pacman -S gimp obs-studio mpv youtube-dl yt-dlp
 
-sudo add-apt-repository ppa:kgilmer/speed-ricer
-sudo apt-get update
-sudo apt install i3-gaps
+#system information
+sudo pacman -S htop neofetch
 
-sudo add-apt-repository ppa:mc3man/mpv-tests
-sudo apt update
-sudo apt install mpv
+#media library and feed interactions
+sudo pacman -S mpd ncmpcpp newsboat 
 
-sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
-sudo chmod a+rx /usr/local/bin/youtube-dl
-
-cd Documents/repositories/
-git clone https://github.com/nlogocntrcaphnt/scripts.git
-cd scripts
-
-for file in /home/polyphemus/Documents/scripts/*; do
-    if [[ $file == *".sh"* ]] 
-	then
-		script="$(echo "${file##*/}")"
-		cp $script /home/polyphemus/
-	fi
-done
-
-mkdir ~/Pictures/screenshots
-mkdir ~/Pictures/screenshots_mpv
-
-sudo dpkg --add-architecture i386
-wget -nc https://dl.winehq.org/wine-builds/winehq.key
-sudo apt-key add winehq.key
-
-sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main' 
-sudo apt update
-sudo apt install --install-recommends winehq-staging
-
-cd Downloads
-git clone https://github.com/doitsujin/dxvk.git
-cd dxvk
-export WINEPREFIX=~/.wine
-./setup_dxvk.sh install
-
-#enable bitmap fonts
-sudo rm /etc/fonts/conf.d/70-no-bitmaps.conf
-dpkg-reconfigure fontconfig-config
-dpkg-reconfigure fontconfig
-fc-cache -f
-
-cd
-cd Documents/repositories
-git clone https://github.com/nlogocntrcaphnt/Homepage.git
+#ancillaries
+sudo pacman -S yay
+yay -S task-spooler soulseekqt
